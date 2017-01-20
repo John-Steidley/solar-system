9146db8e15dd8728e20281b279e99c9e6b63ff8d
John Steidley <johnsteidley@gmail.com>
2017-01-20 17:38:23 +0800

    I found the moon

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,54 @@ extern crate kiss3d;
 extern crate nalgebra as na;
 
 use na::Vector3;
-use kiss3d::window::Window;
-use kiss3d::light::Light;
-
 fn main() {
-    let mut window = Window::new("Kiss3d: cube");
-    window.set_light(Light::StickToCamera);
+    let mut window = kiss3d::window::Window::new("Solar system");
+    window.set_light(kiss3d::light::Light::StickToCamera);
 
-    let sun_size = 1.0f32;
-    let mut sun = window.add_sphere(sun_size);
-    sun.set_color(1.0, 1.0, 0.0);
+    let sun_radius = 0.1f32; // arbitrary unit to fit in initial camera view
+    let mut sun = window.add_sphere(sun_radius);
+    sun.set_color(0.95, 0.7, 0.0);
 
-    let planet_size = sun_size / 30.0;
-    let mut planet = sun.add_sphere(planet_size);
-    let distance_sun_to_planet = sun_size * 10.0;
-    planet.prepend_to_local_translation(&Vector3::new(distance_sun_to_planet, 0.0, 0.0));
+    let venus_radius = sun_radius * 0.25;
+    let mut venus = window.add_sphere(venus_radius);
+    venus.set_color(1.0, 0.3, 0.0);
 
-    let moon_size = planet_size;
-    let mut moon = planet.add_sphere(moon_size);
-    let distance_planet_to_moon = planet_size * 3.0;
-    moon.prepend_to_local_translation(&Vector3::new(distance_planet_to_moon, 0.0, 0.0));
+    let earth_radius = sun_radius * 0.25;
+    let mut earth = window.add_sphere(earth_radius);
+    earth.set_color(0.0, 0.8, 1.0);
 
-    let mut counter = 0;
-    while window.render() {
-        sun.prepend_to_local_rotation(&Vector3::new(0.0f32, 0.0005, 0.0));
+    let moon_radius = earth_radius * 0.25;
+    let mut moon = window.add_sphere(moon_radius);
+    moon.set_color(0.2, 0.2, 0.2);
 
-        if counter == 200 {
-            counter = 0;
-        } else {
-            counter += 1;
+    let distance_sun_to_venus = sun_radius * 3.;
+    let distance_sun_to_earth = sun_radius * 5.0;
+    let distance_earth_to_moon = earth_radius * 2.0;
+
+    let mut day = 1;
+    loop {
+        day += 1;
+        let sun_coords = Vector3::new(0.0, 0.0, 0.0);
+
+        let venus_coords = get_coordinates(day, 3000, distance_sun_to_venus, &sun_coords);
+        let earth_coords = get_coordinates(day, 5000, distance_sun_to_earth, &sun_coords);
+        let moon_coords = get_coordinates(day, 500, distance_earth_to_moon, &earth_coords);
+
+        venus.set_local_translation(venus_coords);
+        earth.set_local_translation(earth_coords);
+        moon.set_local_translation(moon_coords);
+
+        if !window.render() {
+            break;
         }
-        let factor = counter as f32 / 200.0;
-
-        let red = 0.0;
-        let green = factor;
-        let blue = 1.0 - factor;
-        planet.prepend_to_local_rotation(&Vector3::new(0.0f32, 0.002, 0.0));
-        planet.set_color(red, green, blue);
-        moon.set_color(0.2, 0.2, 0.2);
     }
+}
+
+fn get_coordinates(day: u64, orbit_period: u64, scale: f32, origin: &Vector3<f32>) -> Vector3<f32> {
+    let fraction = (day % orbit_period) as f32 / orbit_period as f32;
+    let in_rads = fraction * std::f32::consts::PI * 2.0f32;
+    let x = in_rads.sin() * scale;
+    let y = 0.0;
+    let z = in_rads.cos() * scale;
+    return Vector3::new(x, y, z) + *origin;
 }
